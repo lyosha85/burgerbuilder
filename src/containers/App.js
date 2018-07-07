58aa54ac28c57519e8dd1b8494a6aca9181e4955
Alex <lyosha85@gmail.com>
2018-07-06 22:09:25 -0700

    Clear app and create reference material

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,80 +1,9 @@
 import React, { Component } from "react";
-import People from "../components/People/People";
-import Cockpit from "../components/Cockpit/Cockpit";
-import classes from "./App.css";
-import Aux from "../hoc/Aux";
-import withClass from "../hoc/withClass";
-
-export const AuthContext = React.createContext(false);
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      people: [
-        { id: 0, name: "Alex", age: 22 },
-        { id: 1, name: "Manu", age: 23 },
-        { id: 2, name: "Sarah", age: 24 }
-      ],
-      otherState: "some other state",
-      showPeople: true,
-      toggleClickedCount: 0,
-      authenticated: false
-    };
-  }
-
-  handleNameChange = (event, id) => {
-    const personIndex = this.state.people.findIndex(p => {
-      return p.id === id;
-    });
-    const person = { ...this.state.people[personIndex] };
-    person.name = event.target.value;
-    const people = [...this.state.people];
-    people[personIndex] = person;
-    this.setState({ people: people });
-  };
-
-  handlePeopleToggle = () => {
-    this.setState((prevState, props) => {
-      return {
-        showPeople: !prevState.showPeople,
-        toggleClickedCount: prevState.toggleClickedCount + 1
-      };
-    });
-  };
-
-  deletePersonHandler = index => {
-    const people = [...this.state.people];
-    people.splice(index, 1);
-    this.setState({ people: people });
-  };
-
-  loginHandler = () => {
-    this.setState({ authenticated: true });
-  };
-
   render() {
-    return (
-      <Aux>
-        <Cockpit
-          appTitle={this.props.title}
-          people={this.state.people}
-          clicked={this.handlePeopleToggle}
-          showPeople={this.state.showPeople}
-          login={this.loginHandler}
-        />
-        <AuthContext.Provider value={this.state.authenticated}>
-          {this.state.showPeople && (
-            <People
-              people={this.state.people}
-              changed={this.handleNameChange}
-              clicked={this.deletePersonHandler}
-            />
-          )}
-        </AuthContext.Provider>
-      </Aux>
-    );
+    return <h1> Welcome! </h1>;
   }
 }
 
-export default withClass(App, classes.App);
+export default App;
