bbe593a98373f65c8d3083c84c838e17685bc4f4
Alex <lyosha85@gmail.com>
2018-07-06 23:31:15 -0700

    Add BurgerBuilder placeholders and add a style to Layout

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from "react";
 import Layout from "../components/Layout";
+import BurgerBuilder from "../containers/BurgerBuilder";
 
 class App extends Component {
   render() {
     return (
       <Layout>
-        <p> Hi </p>
+        <BurgerBuilder />
       </Layout>
     );
   }
