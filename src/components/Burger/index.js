b996fc4faec91e8b6bdbd4a98049c8adc9e6c7c9
Alex <lyosha85@gmail.com>
2018-07-07 01:42:09 -0700

    Transform ingridents into jsx elements

--- src/components/Burger/index.js
+++ src/components/Burger/index.js
@@ -4,11 +4,20 @@ import BurgerIngredient from "./BurgerIngredient";
 import classes from "./Burger.css";
 
 const burger = props => {
-  let transformedIngredients = Object.keys(props.ingredients).map(igKey => {
-    return [...Array(props.ingredients[igKey])].map((_, i) => {
-      return <BurgerIngredient key={igKey + i} type={igKey} />;
-    });
-  });
+  let transformedIngredients = Object.keys(props.ingredients)
+    .map(igKey => {
+      return [...Array(props.ingredients[igKey])].map((_, i) => {
+        return <BurgerIngredient key={igKey + i} type={igKey} />;
+      });
+    })
+    .reduce((arr, el) => {
+      // flatten the array
+      return arr.concat(el);
+    }, []);
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p> Please add Ingredints </p>;
+  }
+
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
